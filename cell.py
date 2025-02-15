06e54ae0683d3105e102052756ac2cbfba6a94fe
EilaDenman <eila.denman@ru.nl>
2025-02-15 14:38:40 +0100

    elp

---
+++ cell.py
@@ -0,0 +1,7 @@
+class Cell:
+    def __init__(self, grid_pos, reward, img, is_terminal = False, is_solid = False):
+        self.grid_pos = grid_pos
+        self.reward = reward
+        self.img = img
+        self.is_terminal = is_terminal 
+        self.is_solid = is_solid
