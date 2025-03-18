29fd04484bb8582e14b2c82dd87513261be01acb
Max Groenendaal <mr.groenendaal@student.avans.nl>
2025-03-18 16:36:59 +0100

    Add: TD(0) and TD Sarsa

--- agent.py
+++ agent.py
@@ -1,15 +1,17 @@
+from cell import Cell
 from policies.policy_iteration_policy import PolicyIterationPolicy
+from policies.td_sarsa_policy import TDSarsaPolicy
 from policies.value_iteration_policy import ValueIterationPolicy
 from policies.policy import Policy
 from policies.mcp_policy import MonteCarloPredictionPolicy
 from policies.mcc_policy import MonteCarloControlPolicy
 
 class Agent:
-    def __init__(self, grid: dict[tuple[int, int]], grid_pos: tuple[int, int] = (0, 0), grid_size: int = 5, img: str = "robot.png"):
+    def __init__(self, grid: dict[tuple[int, int], Cell], grid_pos: tuple[int, int] = (0, 0), grid_size: int = 5, img: str = "robot.png"):
         self.img = img
         self.grid_pos = grid_pos
 
-        self.policy: Policy = MonteCarloControlPolicy(grid_pos, grid, grid_size)
+        self.policy: Policy = TDSarsaPolicy(grid_pos, grid, grid_size)
         self.last_reward_val = 0
 
     def action(self) -> tuple[int, int]:
